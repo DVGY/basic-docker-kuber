3f5c7a2791bc24e005ec5484d54f735850b79fb9
DVGY <gaurav.gydv@gmail.com>
2021-04-28 12:58:39 +0530

    nice

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -7,7 +7,7 @@ if (!process.env.JWT_SECRET_KEY) {
 if (!process.env.MONGO_URI) {
   throw new Error('MONGO URI is not defined');
 }
-
+console.log('Check auth depl workflows');
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
