fa238da619b8dbf858d7f7c2750d976746ab75af
DVGY <gaurav.gydv@gmail.com>
2021-04-28 14:21:44 +0530

    modify console log

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -7,7 +7,7 @@ if (!process.env.JWT_SECRET_KEY) {
 if (!process.env.MONGO_URI) {
   throw new Error('MONGO URI is not defined');
 }
-console.log('Check auth depl workflows');
+console.log('Starting inside digital ocean');
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
